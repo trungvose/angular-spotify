b74c3df6e2d13c3e98b2812d3acadcfb51cda368
Trung Vo <trungk18@gmail.com>
2021-03-24 21:24:11 +0800

    feat: add loading selector

--- libs/web/shared/data-access/store/src/lib/playlists/playlists.selector.ts
+++ libs/web/shared/data-access/store/src/lib/playlists/playlists.selector.ts
@@ -4,6 +4,10 @@ import { RootState } from '../rootState';
 //Playlists
 export const getPlaylistsState = (state: RootState) => state.playlists;
 export const getPlaylists = createSelector(getPlaylistsState, (state) => state.data);
+export const getIsPlaylistsLoading = createSelector(
+  getPlaylistsState,
+  (state) => state.status === 'loading'
+);
 export const getPlaylistsMap = createSelector(getPlaylistsState, (state) => state.map);
 export const getPlaylist = (playlistId: string) =>
   createSelector(getPlaylistsMap, (map) => map?.get(playlistId));
