9c435d931982c76f52c60c53f417c87e267b75e7
Trung Vo <trungk18@gmail.com>
2021-03-25 23:24:49 +0800

    feat: fix playlist loadings, netlify refresh and skeleton height

--- libs/web/collection/playlists/src/lib/playlists.component.ts
+++ libs/web/collection/playlists/src/lib/playlists.component.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { select, Store } from '@ngrx/store';
+import { PlayerApiService } from '@angular-spotify/web/shared/data-access/spotify-api';
 import {
   getPlaylists,
-  loadPlaylists,
+  getPlaylistsLoading,
   RootState
 } from '@angular-spotify/web/shared/data-access/store';
-import { Observable } from 'rxjs';
 import { RouteUtil } from '@angular-spotify/web/util';
-import { PlayerApiService } from '@angular-spotify/web/shared/data-access/spotify-api';
-import { getPlaylistsLoading } from '@angular-spotify/web/shared/data-access/store';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'as-playlists',
   templateUrl: './playlists.component.html',
   styleUrls: ['./playlists.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlaylistsComponent implements OnInit {
+export class PlaylistsComponent {
   isPlaylistsLoading$: Observable<boolean>;
   playlists$: Observable<SpotifyApi.ListOfUsersPlaylistsResponse | null>;
 
   constructor(private store: Store<RootState>, private playerApi: PlayerApiService) {
     this.playlists$ = this.store.pipe(select(getPlaylists));
     this.isPlaylistsLoading$ = this.store.pipe(select(getPlaylistsLoading));
-  }
-
-  ngOnInit(): void {
-    this.store.dispatch(loadPlaylists());
   }
 
   getPlaylistRouteUrl(playlist: SpotifyApi.PlaylistObjectSimplified) {
