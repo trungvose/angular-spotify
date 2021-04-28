943def30a7d7ed5b5eb0e0424b56bf29e2520203
Trung Vo <trungk18@gmail.com>
2021-04-28 09:48:08 +0800

    feat: force update PWA

--- apps/angular-spotify/src/app/app.component.ts
+++ apps/angular-spotify/src/app/app.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { GoogleAnalyticsService } from './google-analytics.service';
+import { PromptUpdateService } from './promp-update.service';
 
 @Component({
   selector: 'angular-spotify-root',
   template: ` <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private router: Router, private googleAnalytics: GoogleAnalyticsService) {
+export class AppComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private googleAnalytics: GoogleAnalyticsService,
+    private promptUpdate: PromptUpdateService
+  ) {}
+
+  ngOnInit() {
     if (environment.production) {
-      this.router.events
-        .pipe(filter((event) => event instanceof NavigationEnd))
-        .subscribe((navigationEndEvent) => {
-          this.googleAnalytics.sendPageView(
-            (navigationEndEvent as NavigationEnd).urlAfterRedirects
-          );
-        });
+      this.initGoogleAnalytics();
+      this.initForceUpdate();
     }
+  }
+
+  private initGoogleAnalytics() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((navigationEndEvent) => {
+        this.googleAnalytics.sendPageView((navigationEndEvent as NavigationEnd).urlAfterRedirects);
+      });
+  }
+  
+  private initForceUpdate() {
+    this.promptUpdate.forceUpdate().subscribe();
   }
 }
