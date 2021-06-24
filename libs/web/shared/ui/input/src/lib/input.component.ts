7696899c48d7c2b0307f53056c13810c3209563e
Nikos Anifantis <nikosanif@gmail.com>
2021-06-24 15:05:18 +0300

    feat(input): add component at input lib

---
+++ libs/web/shared/ui/input/src/lib/input.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+type IconSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+const IconSizesInPx: { [key in IconSize]: number } = {
+  xs: 8, // 0.5rem
+  sm: 12, // 0.75rem
+  md: 16, // 1rem
+  lg: 24, // 1.5rem
+  xl: 32, // 2rem
+  xxl: 40 // 2.5rem
+};
+
+@Component({
+  selector: 'as-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent {
+  @Input() control = new FormControl('');
+  @Input() containerClassName = '';
+  @Input() icon = '';
+  @Input() iconSize: IconSize = 'md';
+  @Input() placeholder = '';
+  @Input() rounded = false;
+  @Input() enableClearButton = false;
+
+  get iconContainerWidth(): number {
+    return IconSizesInPx[this.iconSize] * 2;
+  }
+
+  get inputContainerHeight(): number {
+    return IconSizesInPx[this.iconSize] * 1.5;
+  }
+
+  get showClearButton(): boolean {
+    return this.enableClearButton && this.control?.value;
+  }
+
+  clear() {
+    this.control.patchValue('');
+  }
+}
