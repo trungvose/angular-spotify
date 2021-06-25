3a6f09118fd24b013436913882598e362bcbcbf1
Nikos Anifantis <nikosanif@gmail.com>
2021-06-25 13:51:38 +0300

    refactor: change order at nav items

--- libs/web/shared/data-access/store/src/lib/ui/ui-store.ts
+++ libs/web/shared/data-access/store/src/lib/ui/ui-store.ts
@@ -16,8 +16,8 @@ export class UIStore extends ComponentStore<UIState> {
     super({
       navItems: [
         { label: 'Home', path: '', exact: true },
-        { label: 'Browse', path: '/browse' },
         { label: 'Search', path: '/search' },
+        { label: 'Browse', path: '/browse' },
         { label: 'My Playlists', path: '/collection/playlists' },
         { label: 'My Albums', path: '/albums' }
       ],
