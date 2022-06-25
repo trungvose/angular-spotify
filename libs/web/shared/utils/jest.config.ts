9d90ddccb97d444f781633a40ee06817f1b00752
Trung Vo <trungk18@gmail.com>
2022-06-25 10:24:32 +0800

    chore: [nx migration] update-jest-config-extensions

---
+++ libs/web/shared/utils/jest.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'web-shared-utils',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' }
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/web/shared/utils'
+};
