dad2a2353699b1c76578cf9b220379b1df569b40
Trung Vo <trungk18@gmail.com>
2023-04-23 12:15:36 +0800

    chore: [nx migration] ngrx-component-migration-15-beta

--- libs/web/visualizer/ui/src/lib/web-visualizer-ui.module.ts
+++ libs/web/visualizer/ui/src/lib/web-visualizer-ui.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SvgIconsModule } from '@ngneat/svg-icon';
-import { ReactiveComponentModule } from '@ngrx/component';
+import { LetModule, PushModule } from '@ngrx/component';
 import { WebVisualizerUiComponent } from './web-visualizer-ui.component';
 
 @NgModule({
-  imports: [CommonModule, SvgIconsModule, ReactiveComponentModule],
+  imports: [CommonModule, SvgIconsModule, LetModule, PushModule],
   declarations: [WebVisualizerUiComponent],
   exports: [WebVisualizerUiComponent]
 })
