f30efa1c71724e5c76d4948b8685f5cdf4764289
Trung Vo <trungk18@gmail.com>
2023-04-23 12:14:58 +0800

    chore: [nx migration] update-configs-jest-29

--- libs/web/shared/app-init/jest.config.ts
+++ libs/web/shared/app-init/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'web-shared-app-init',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$'
-    }
-  },
+  globals: {},
   coverageDirectory: '../../../../coverage/libs/web/shared/app-init',
   transform: {
-    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular'
+    '^.+.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
   },
   transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
