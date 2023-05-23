e07af31f6f971865a711779699cbdcae9b433f2e
Trung Vo <trungk18@gmail.com>
2023-05-23 21:45:12 +0800

    refactor(wdc-2023): add missing times, expand and shrink icons

--- libs/web/shared/ui/icon/src/lib/icon.module.ts
+++ libs/web/shared/ui/icon/src/lib/icon.module.ts
@@ -1,70 +1,76 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { SvgIconsModule } from '@ngneat/svg-icon';
+import { asAudioAnimatedIcon } from './svg/audio-animated';
+import { asCaretDownFillIcon } from './svg/caret-down-fill';
+import { asClockIcon } from './svg/clock';
+import { asCompassIcon } from './svg/compass';
+import { asCompassFillIcon } from './svg/compass-fill';
+import { asCupIcon } from './svg/cup';
+import { asCupFillIcon } from './svg/cup-fill';
+import { asCupStrawIcon } from './svg/cup-straw';
+import { asEmojiHeartEyesIcon } from './svg/emoji-heart-eyes';
+import { asExpandIcon } from './svg/expand';
+import { asGithubIcon } from './svg/github';
+import { asHeartIcon } from './svg/heart';
+import { asHeartFillIcon } from './svg/heart-fill';
 import { asHomeIcon } from './svg/home';
-import { asStepBackwardIcon } from './svg/step-backward';
-import { asStepForwardIcon } from './svg/step-forward';
+import { asHouseDoorIcon } from './svg/house-door';
+import { asHouseDoorFillIcon } from './svg/house-door-fill';
+import { asJournalIcon } from './svg/journal';
+import { asMusicNoteBeamedIcon } from './svg/music-note-beamed';
+import { asMusicNoteListIcon } from './svg/music-note-list';
 import { asPauseIcon } from './svg/pause';
 import { asPlayIcon } from './svg/play';
+import { asQuestionCircleIcon } from './svg/question-circle';
+import { asSearchIcon } from './svg/search';
+import { asSearchHeartIcon } from './svg/search-heart';
+import { asShrinkIcon } from './svg/shrink';
+import { asStepBackwardIcon } from './svg/step-backward';
+import { asStepForwardIcon } from './svg/step-forward';
+import { asTimesIcon } from './svg/times';
+import { asTwitterIcon } from './svg/twitter';
 import { asVolumeHighIcon } from './svg/volume-high';
 import { asVolumeMediumIcon } from './svg/volume-medium';
 import { asVolumeMuteIcon } from './svg/volume-mute';
-import { asClockIcon } from './svg/clock';
-import { asAudioAnimatedIcon } from './svg/audio-animated';
-import { asGithubIcon } from './svg/github';
-import { asTwitterIcon } from './svg/twitter';
-import { asQuestionCircleIcon } from './svg/question-circle';
-import { asCupIcon } from './svg/cup';
-import { asEmojiHeartEyesIcon } from './svg/emoji-heart-eyes';
-import { asCupFillIcon } from './svg/cup-fill';
-import { asCupStrawIcon } from './svg/cup-straw';
-import { asCaretDownFillIcon } from './svg/caret-down-fill';
-import { asSearchIcon } from './svg/search';
-import { asCompassIcon } from './svg/compass';
-import { asCompassFillIcon } from './svg/compass-fill';
-import { asHouseDoorIcon } from './svg/house-door';
-import { asHouseDoorFillIcon } from './svg/house-door-fill';
-import { asHeartIcon } from './svg/heart';
-import { asHeartFillIcon } from './svg/heart-fill';
-import { asSearchHeartIcon } from './svg/search-heart';
-import { asMusicNoteListIcon } from './svg/music-note-list';
-import { asJournalIcon } from './svg/journal';
-import { asMusicNoteBeamedIcon } from './svg/music-note-beamed';
 
 @NgModule({
   imports: [
     CommonModule,
     SvgIconsModule.forRoot({
       icons: [
-        asHomeIcon,
-        asStepBackwardIcon,
-        asStepForwardIcon,
-        asPauseIcon,
-        asPlayIcon,
-        asVolumeHighIcon,
-        asVolumeMediumIcon,
-        asVolumeMuteIcon,
-        asClockIcon,
         asAudioAnimatedIcon,
-        asGithubIcon,
-        asTwitterIcon,
-        asQuestionCircleIcon,
-        asCupIcon,
-        asCupFillIcon,
-        asCupStrawIcon,
-        asEmojiHeartEyesIcon,
         asCaretDownFillIcon,
-        asSearchIcon,
+        asClockIcon,
         asCompassFillIcon,
         asCompassIcon,
-        asHouseDoorIcon,
-        asHouseDoorFillIcon,
-        asHeartIcon,
+        asCupFillIcon,
+        asCupIcon,
+        asCupStrawIcon,
+        asEmojiHeartEyesIcon,
+        asExpandIcon,
+        asGithubIcon,
         asHeartFillIcon,
-        asSearchHeartIcon,
-        asMusicNoteListIcon,
+        asHeartIcon,
+        asHomeIcon,
+        asHouseDoorFillIcon,
+        asHouseDoorIcon,
         asJournalIcon,
-        asMusicNoteBeamedIcon
+        asMusicNoteBeamedIcon,
+        asMusicNoteListIcon,
+        asPauseIcon,
+        asPlayIcon,
+        asQuestionCircleIcon,
+        asSearchHeartIcon,
+        asSearchIcon,
+        asShrinkIcon,
+        asStepBackwardIcon,
+        asStepForwardIcon,
+        asTimesIcon,
+        asTwitterIcon,
+        asVolumeHighIcon,
+        asVolumeMediumIcon,
+        asVolumeMuteIcon
       ]
     })
   ],
