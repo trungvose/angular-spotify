e8aafc3695ef1c218f67f858f6bbf6ebc5348d42
Trung Vo <trungk18@gmail.com>
2024-04-12 09:53:48 +0800

    feat: add view transition for playlist

--- libs/web/shared/ui/playlist-list/src/lib/playlist-list.component.ts
+++ libs/web/shared/ui/playlist-list/src/lib/playlist-list.component.ts
@@ -1,6 +1,8 @@
+import { CurrentViewTransitionService, getViewTransitionParamValue } from '@angular-spotify/shared/view-transition';
 import { PlaylistsResponseWithRoute } from '@angular-spotify/web/shared/data-access/models';
 import { PlayerApiService } from '@angular-spotify/web/shared/data-access/spotify-api';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RouterUtil } from '@angular-spotify/web/shared/utils';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 
 @Component({
   selector: 'as-playlist-list',
@@ -11,6 +13,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class PlaylistListComponent {
   @Input() playlists!: PlaylistsResponseWithRoute | null;
   @Input() isLoading!: boolean | null;
+  transitionService = inject(CurrentViewTransitionService);
 
   constructor(private playerApi: PlayerApiService) {}
 
@@ -21,4 +24,18 @@ export class PlaylistListComponent {
       })
       .subscribe();
   }
+
+  /*
+    When transitioning to or from the detail page, include the `with-view-transition` transition name.
+    This enables the browser to animate between the cover photo image in the list and its image on the detail page.
+  */
+    viewTransitionName(item: SpotifyApi.PlaylistObjectSimplified) {
+      const transition = this.transitionService.currentTransition();
+      const transitionAlbumId = getViewTransitionParamValue(
+        transition,
+        RouterUtil.Configuration.PlaylistId
+      );
+      const withViewTransition = transitionAlbumId === item.id;
+      return withViewTransition ? 'with-view-transition' : '';
+    }
 }
