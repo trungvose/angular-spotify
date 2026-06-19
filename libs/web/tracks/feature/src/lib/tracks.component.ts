758acb81859f50903960c0abc37f410644b43aac
Trung Vo <trungk18@gmail.com>
2026-06-19 11:51:11 +0800

    feat(tracks): seed liked state and drop un-liked rows on Liked Songs

--- libs/web/tracks/feature/src/lib/tracks.component.ts
+++ libs/web/tracks/feature/src/lib/tracks.component.ts
@@ -1,5 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  OnInit
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TracksStore } from '@angular-spotify/web/tracks/data-access';
+import { SavedTracksStore } from '@angular-spotify/web/shared/data-access/store';
+
 @Component({
   selector: 'as-tracks',
   templateUrl: './tracks.component.html',
@@ -10,10 +20,35 @@ import { TracksStore } from '@angular-spotify/web/tracks/data-access';
 export class TracksComponent implements OnInit {
   vm$ = this.store.vm$;
 
-  constructor(private store: TracksStore) {}
+  displayedTracks$ = combineLatest([
+    this.store.vm$,
+    this.savedTracksStore.savedMap$
+  ]).pipe(
+    map(([vm, savedMap]) =>
+      (vm.data ?? []).filter(
+        (item) => item.track && savedMap[item.track.id] !== false
+      )
+    )
+  );
+
+  constructor(
+    private store: TracksStore,
+    private savedTracksStore: SavedTracksStore,
+    private destroyRef: DestroyRef
+  ) {}
 
   ngOnInit() {
     this.store.loadTracks();
+    this.store.vm$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((vm) => {
+        const ids = (vm.data ?? [])
+          .map((item) => item.track?.id)
+          .filter((id): id is string => !!id);
+        if (ids.length) {
+          this.savedTracksStore.markSaved(ids);
+        }
+      });
   }
 
   playTrack(track: SpotifyApi.TrackObjectFull) {
