3f15cd9797f2a75450e88264418228414a845ab1
Trung Vo <trungk18@gmail.com>
2026-06-19 11:46:25 +0800

    feat(now-playing-bar): show like toggle for the current track

--- libs/web/shell/ui/now-playing-bar/src/lib/now-playing-bar.component.ts
+++ libs/web/shell/ui/now-playing-bar/src/lib/now-playing-bar.component.ts
@@ -1,14 +1,35 @@
 import { PlaybackStore } from '@angular-spotify/web/shared/data-access/store';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SavedTracksStore } from '@angular-spotify/web/shared/data-access/store';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  OnInit
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 @Component({
   selector: 'as-now-playing-bar',
   templateUrl: './now-playing-bar.component.html',
   styleUrls: ['./now-playing-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NowPlayingBarComponent {
+export class NowPlayingBarComponent implements OnInit {
   currentTrack$ = this.playbackStore.currentTrack$;
 
-  constructor(private playbackStore: PlaybackStore) {
+  constructor(
+    private playbackStore: PlaybackStore,
+    private savedTracksStore: SavedTracksStore,
+    private destroyRef: DestroyRef
+  ) {}
+
+  ngOnInit(): void {
+    this.currentTrack$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((track) => {
+        if (track?.id) {
+          this.savedTracksStore.checkSaved([track.id]);
+        }
+      });
   }
 }
